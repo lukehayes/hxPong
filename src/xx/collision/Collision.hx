5b68637cd878721e0b1f35d0a02a8e0e6a6320b8
lukehayes <lukepurplehayes@gmail.com>
2024-09-22 09:50:02 +0100

    Added start of Collision.hx

---
+++ src/xx/collision/Collision.hx
@@ -0,0 +1,10 @@
+package xx.collision;
+
+import xx.Global;
+
+class Collision extends h2d.col.Bounds
+{
+    public function new(x: Int, y: Int, w :Int = Global.TILESIZE, h :Int = Global.TILESIZE)
+    {
+    }
+}
